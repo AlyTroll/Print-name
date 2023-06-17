f48e12aaa98eb6481fe66665941ba9471fa069e0
Ali TROLL <109201113+AlyTroll@users.noreply.github.com>
2023-06-18 00:55:43 +0330

    Create Print.py

---
+++ Print.py
@@ -0,0 +1,10 @@
+import os
+from os import system
+system("pip install rainbowtext")
+system("pip install pyfiglet")
+import rainbowtext,pyfiglet
+name = input("What Name Print? ")
+print(rainbowtext.text(pyfiglet.figlet_format(name)
+print("")
+                       print("")
+                       print("GoodBy")
